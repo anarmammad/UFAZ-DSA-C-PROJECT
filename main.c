48b47589f0f72c66dbb5aac83603ed416e8e387c
Huseyn Mashadiyev <huseyn.m@labrin.tech>
2020-01-02 11:32:38 +0400

    BMP File Reading Structure changed 1. Changed to structural view 2. Combined several views to avoid array issues 3. All work combined to single function

--- main.c
+++ main.c
@@ -7,32 +7,47 @@
 #include "bmp.h"
 #include "helper.h"
 #include "morse.h"
+#include <stddef.h>
 
 
 
 
 int main(int argc, char *argv[]){
-    get_options(argc, argv);
+//    get_options(argc, argv);
+//
+//    printf("Text to be written: %s\n", TEXT_TO_WRITE);
+//    printf("%s\n", IS_DATE_SET ? "Date will be written":"Date won't be written");
+//    printf("Position to put watermark: (%u, %u)\n", POSITION_TO_WRITE[0], POSITION_TO_WRITE[1]);
+//    printf("Output file: %s\n", OUTPUT_FILE);
+//    printf("Color to be written: "); print_color(&COLOR_TO_WRITE);
+//
+//
+//    BMP* bmp_image = open_bmp(FILENAME);
+//
+//    printf("\n\n**********\n\n");
+//    read_first_header(bmp_image);
+//    printf("Image starts at: %u\n", bmp_image->image_starts_at);
+//    printf("File size is: %u\n", bmp_image->filesize);
+//
+//    read_second_header(bmp_image);
+//    printf("Header size is: %u\n", bmp_image->header_size);
+//    printf("Image width is: %u\n", bmp_image->image_width);
+//    printf("Image height is: %u\n", bmp_image->image_height);
+//    printf("Number of color planes is: %u\n", bmp_image->n_color_planes);
+//    printf("Bits per pixel is: %u\n", bmp_image->bits_per_pixel);
 
-    printf("Text to be written: %s\n", TEXT_TO_WRITE);
-    printf("%s\n", IS_DATE_SET ? "Date will be written":"Date won't be written");
-    printf("Position to put watermark: (%u, %u)\n", POSITION_TO_WRITE[0], POSITION_TO_WRITE[1]);
-    printf("Output file: %s\n", OUTPUT_FILE);
-    printf("Color to be written: "); print_color(&COLOR_TO_WRITE);
+    PBMP file = open_bmp("/home/harry/example.bmp");
 
-
-    BMP* bmp_image = open_bmp(FILENAME);
-
-    printf("\n\n**********\n\n");
-    read_first_header(bmp_image);
-    printf("Image starts at: %u\n", bmp_image->image_starts_at);
-    printf("File size is: %u\n", bmp_image->filesize);
-
-    read_second_header(bmp_image);
-    printf("Header size is: %u\n", bmp_image->header_size);
-    printf("Image width is: %u\n", bmp_image->image_width);
-    printf("Image height is: %u\n", bmp_image->image_height);
-    printf("Number of color planes is: %u\n", bmp_image->n_color_planes);
-    printf("Bits per pixel is: %u\n", bmp_image->bits_per_pixel);
+    printf("BMP:\n"
+           "\tFileSize: %d\n"
+           "\tDataOffset: %d\n"
+           "\tSize: %d\n"
+           "\tWidth: %d\n"
+           "\tHeight: %d\n",
+           file->Header.FileSize,
+           file->Header.DataOffset,
+           file->InfoHeader.Size,
+           file->InfoHeader.Width,
+           file->InfoHeader.Height);
     return 0;
 }
